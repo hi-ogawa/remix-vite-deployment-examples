52b6fc1dba481b84ea5c6d49b43319605eb433c1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-25 16:36:39 +0900

    refactor: simplify assets handling for cloudflare pages (#6)

--- misc/cloudflare-pages/build.sh
+++ misc/cloudflare-pages/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -eu -o pipefail
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# static
+mkdir -p build/client
+cp -r ../../build/client/. build/client
+rm -rf .vercel/output/static/.vite
+cp _headers _routes.json build/client
+
 # functions
 mkdir -p "functions"
-rm -f "functions/[[path]].js"
-npx esbuild app/adapters/cloudflare-pages.ts \
-  "--outfile=functions/[[path]].js" \
+rm -f "../../functions/[[path]].js"
+npx esbuild ../../app/adapters/cloudflare-pages.ts \
+  "--outfile=../../functions/[[path]].js" \
   --metafile=build/esbuild-metafile-cloudflare-pages.json \
   --bundle --minify --format=esm --platform=browser
