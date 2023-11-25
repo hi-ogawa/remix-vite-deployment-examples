69ba0a7c977e630f54af0d56c7d82c62311ff505
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-25 23:00:51 +0900

    fix: fix request abort signal error

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="@remix-run/node" />
 /// <reference types="vite/client" />
+
+// copied from https://github.com/rakkasjs/rakkasjs/blob/65481844280a936601c3eb73a00dbb1c8362ea14/packages/rakkasjs/src/types.d.ts#L12-L15
+declare module "react-dom/server.browser" {
+  export * from "react-dom/server";
+}
