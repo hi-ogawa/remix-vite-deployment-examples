d8c29f1b0f00d1398c64cb4415b3dc8868128153
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-25 16:32:48 +0900

    fix: fix build.sh

--- misc/cloudflare-pages/build.sh
+++ misc/cloudflare-pages/build.sh
@@ -12,7 +12,7 @@ cp _headers _routes.json build/client
 # functions
 mkdir -p "functions"
 rm -f "../../functions/[[path]].js"
-npx esbuild app/adapters/cloudflare-pages.ts \
+npx esbuild ../../app/adapters/cloudflare-pages.ts \
   "--outfile=../../functions/[[path]].js" \
   --metafile=build/esbuild-metafile-cloudflare-pages.json \
   --bundle --minify --format=esm --platform=browser
