c68c5adf0896a3bb66832034f51ebb41de01521c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-04 11:02:18 +0900

    refactor: use cloudflare pages "advanced mode"  (#10)

--- misc/cloudflare-pages/build.sh
+++ misc/cloudflare-pages/build.sh
@@ -6,13 +6,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # static
 mkdir -p build/client
 cp -r ../../build/client/. build/client
-rm -rf .vercel/output/static/.vite
+rm -rf build/client/.vite
 cp _headers _routes.json build/client
 
-# functions
-mkdir -p "functions"
-rm -f "../../functions/[[path]].js"
-npx esbuild ../../app/adapters/cloudflare-pages.ts \
-  "--outfile=../../functions/[[path]].js" \
+# functions (advanced mode https://developers.cloudflare.com/pages/platform/functions/advanced-mode/)
+npx esbuild ../../app/adapters/cloudflare-workers.ts \
+  "--outfile=build/client/_worker.js" \
   --metafile=build/esbuild-metafile-cloudflare-pages.json \
   --bundle --minify --format=esm --platform=browser
